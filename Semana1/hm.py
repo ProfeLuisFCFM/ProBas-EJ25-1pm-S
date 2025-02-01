e29b23db9cb81f699d4214e72f9f689112726eca
Salon 111 <cs113invop@gmail.com>
2025-02-01 14:39:38 -0600

    operadores

--- Semana1/hm.py
+++ Semana1/hm.py
@@ -1,1 +1,38 @@
 print("Adios Inmundo")
+
+print("Hola, dime tu nombre: ")
+v = input()
+
+print(f"Hola {v}")
+
+#Relacionales
+#= igualdad
+#!=,<> diferenciales
+#>=,<= desigualdades
+#<,> inecuación
+#Lógicos
+#Y and
+#O or
+#N not
+#Aritméticos
+#P  Parentesis ()
+#E  Exponentees b^2 b**2
+#M  *
+#D  / -> // % 
+#A  +
+#S  -
+
+if True:
+    print("Hola Mundo")
+
+if False:
+    print("Me van a brincar")
+else:
+    print("oook")
+
+if None:
+    print("Esto no seta ejecutado")
+elif False:
+    print("print")
+else:
+    print("Nada")
